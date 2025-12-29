136f2a9867e48fcfd6fee630637cfdfc02b59440
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2025-12-30 00:03:07 +0800

    Update twrp_RMX2185.mk

--- twrp_RMX2185.mk
+++ twrp_RMX2185.mk
@@ -17,27 +17,20 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Inherit from blossom device
+$(call inherit-product, device/realme/RMX2185/device.mk)
+
 PRODUCT_DEVICE := RMX2185
 PRODUCT_NAME := twrp_RMX2185
-PRODUCT_BRAND := realme
-PRODUCT_MODEL := RMX2185
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := RM6765-G35 Series
 PRODUCT_MANUFACTURER := realme
-
-PRODUCT_SHIPPING_API_LEVEL := 29
-
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock
-
-# Bypass anti-rollback ROMs protection
-# Set build date to Jan 1 2009 00:00:00
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.date.utc=1230768000
